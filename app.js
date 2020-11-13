92172b36b85d0bd415aa3f0700be6c8bb31afe8d
Alchemistblue <alchemistcodepython@gmail.com>
2020-11-13 14:36:16 -0600

    Automatice el hecho de si alguien duplica, triplica o n cantidad de tarjetas, este se repita n veces y se genere de forma random con la funcion newDeck, para que solo cuando de f5, las cartas se vuelvan a repartir de forma random

--- app.js
+++ app.js
@@ -1,23 +1,40 @@
-// declaro los nombres para llamar despues
+// card names
 const emptyCards = ['blank','white'];
-const cardNames = ['cheeseburger','fries','hotdog','ice-cream','milkshake','pizza']
-// Repito dos veces el arreglo
-let cardArray = cardNames.concat(cardNames);// aun no se lo agrego
-// donde estará el tablero
-const grid = document.querySelector('.grid')
-const resultDisplay = document.querySelector('#result')// donde muestro resultado
-// Funcion donde creare el tablero con las respectivas tarjetas
+const cardNames = ['fries','cheeseburger','ice-cream','hotdog','milkshake','pizza'];
+// where the board its gonna be
+const grid = document.querySelector('.grid');
+// create the deck of all cards that i gonna put in the board
+let deck = [];
+// this function check the lenght of cards, store it and returned sort
+function newDeck (){
+    let totalCards = document.getElementsByClassName("animation").length;
+    let cont = 0;
+    while(cont < totalCards/cardNames.length){
+        ++cont;
+        for(let card of cardNames){
+            deck.push(card)
+        }
+    }
+    deck.sort(()=>.5-Math.random());
+    return deck;
+}
+// creating the board
 function createBoard(){
-    for(let i=0; i < cardNames.length*2; i++){
-        // creo la tarjeta
+    for(let i = 0; i < 18; i++){
         let card = document.createElement('img');
-        // configuro todas las tarjetas con los sig atributos
         card.src = `images/${emptyCards[0]}.png`;
-        card.setAttribute('class','animation');
+        card.setAttribute(`class`,'animation');
         card.id = `card-${i}`;
+        card.addEventListener('click',flipCard)
         grid.appendChild(card);
     }
 }
-
-// llamamos a la función
+// calling the functions
 createBoard();
+newDeck();
+
+function flipCard(){
+    let getId = parseInt(this.getAttribute("id").slice(5,7));
+    this.setAttribute('src',`images/${deck[getId]}.png`)
+    console.log(deck)
+}
